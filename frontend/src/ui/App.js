ca0cd6abcf9751e2102a73dadf7ae60a7cdfdb53
Jonathan Harada <JHarada@cnm.edu>
2021-08-16 15:53:20 -0600

    created header static page

--- frontend/src/ui/App.js
+++ frontend/src/ui/App.js
@@ -3,11 +3,14 @@ import { Route, Switch } from 'react-router'
 import { Home } from './Home'
 import { FourOhFour } from './FourOhFour'
 import React from 'react'
+import {Header} from "./Header";
+
 
 export const App = () => (
     <>
         <BrowserRouter>
             <Switch>
+                <Route exact path='/header' component={Header} />
                 <Route exact path='/' component={Home} />
                 <Route component={FourOhFour} />
             </Switch>

18bcd90d104bb428c0f4e688a84abebec13861bf
Anais Del campo <adelcampo@cnm.edu>
2021-08-16 10:53:33 -0600

    search updated

--- frontend/src/ui/Search.js
+++ frontend/src/ui/Search.js
@@ -14,29 +14,7 @@ export const Search = () => {
 
             </Container>
 
-            <Container fluid>
-                <Card>
-                    <Card.Header className="text-center">Scholarship Search Results</Card.Header>
 
-                    <Row>
-                        <Col sm={1} >
-                            <Star className="m-5" size={50}/>
-                        </Col>
-                        <Col>
-                            <Card.Body>
-                                <Card.Title>Name of school</Card.Title>
-                                <Card.Text>
-                                    Name of Scholarship with description, criteria, deadline and reward amount
-                                    *Link to actual website
-                                </Card.Text>
-                            </Card.Body>
-                        </Col>
-
-
-                    </Row>
-
-                </Card>
-            </Container>
 
         </>
     )

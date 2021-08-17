bcfd8f3a2870b15f2f0120a4f8b20bca35fa7995
Komal Chaudhary <kchaudhary1@cnm.edu>
2021-08-17 10:38:25 -0600

    created Homepage

--- frontend/src/ui/App.js
+++ frontend/src/ui/App.js
@@ -3,12 +3,15 @@ import { Route, Switch } from 'react-router'
 import { Home } from './Home'
 import { FourOhFour } from './FourOhFour'
 import React from 'react'
+import  {UserHome} from "./UserHome";
+import "./syle.css"
 
 export const App = () => (
     <>
         <BrowserRouter>
             <Switch>
                 <Route exact path='/' component={Home} />
+                <Route exact path='/userhome' component={UserHome} />
                 <Route component={FourOhFour} />
             </Switch>
         </BrowserRouter>

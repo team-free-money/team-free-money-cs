ef680211e55f5cad0bc17b42cf31fc81458cfe2e
gabriela-reyes <86327609+gabriela-reyes@users.noreply.github.com>
2021-08-16 18:45:08 -0600

    added login form

--- frontend/src/ui/App.js
+++ frontend/src/ui/App.js
@@ -3,12 +3,14 @@ import { Route, Switch } from 'react-router'
 import { Home } from './Home'
 import { FourOhFour } from './FourOhFour'
 import React from 'react'
+import {UserLogin} from "./UserLogin";
 
 export const App = () => (
     <>
         <BrowserRouter>
             <Switch>
                 <Route exact path='/' component={Home} />
+                <Route exact path='/login' component={UserLogin} />
                 <Route component={FourOhFour} />
             </Switch>
         </BrowserRouter>

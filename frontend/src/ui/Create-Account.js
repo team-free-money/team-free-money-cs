3be919a7ac58510f728945c8a40e94bd8aa77c23
gabriela-reyes <86327609+gabriela-reyes@users.noreply.github.com>
2021-08-16 18:33:20 -0600

    added create account form

--- frontend/src/ui/Create-Account.js
+++ frontend/src/ui/Create-Account.js
@@ -1,20 +1,36 @@
 import React from "react";
-import {Form} from "react-bootstrap";
+import {Button, CloseButton, Col, Container, Form, Row} from "react-bootstrap";
 
 export const CreateAccount = () => {
     return (
         <>
-        <h1>Is this thing on?</h1>
-            <Form>
-                <Form.Group className="mb-3" controlId="formGroupEmail">
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="formGroupPassword">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
-                </Form.Group>
-            </Form>
+            <h1 className="text-center">Create an Account</h1>
+            <h6 className="text-center">Access exclusive rankings & reviews, apply for scholarships, and more.</h6>
+            <Container className="border border-dark border-3 rounded bg-secondary">
+                <Form>
+                    <Row>
+                        <Col>
+                            <CloseButton></CloseButton>
+                            <h3>Email:</h3>
+                            <Form.Control placeholder="Please use valid email"/>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <h3>Password:</h3>
+                            <Form.Control placeholder="********" />
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col className="text-center">
+                            <Button variant="dark">Continue</Button>{' '}
+                        </Col>
+                    </Row>
+                    <Row className="justify-content-center p-5">
+                        <h6>Already have an account? Sign-in instead.</h6>
+                    </Row>
+                </Form>
+            </Container>
         </>
     )
 }

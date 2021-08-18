d68ac9a53c8d41556e2c5be8094350a3a88c9997
Jonathan Harada <JHarada@cnm.edu>
2021-08-18 16:19:25 -0600

    added navigation buttons

--- frontend/src/ui/Navbar.js
+++ frontend/src/ui/Navbar.js
@@ -7,19 +7,19 @@ export function Navigation () {
     return (
         <>
             <Container>
-            <Navbar bg="light" expand="lg">
+            <Navbar bg="light" expand="lg" className="border border-dark rounded">
                 <Container>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="">
                             <Nav.Link href="/search">Native American</Nav.Link>
-                            <Nav.Link href="#link">Hispanic</Nav.Link>
-                            <Nav.Link href="#link">New Student</Nav.Link>
-                            <Nav.Link href="/">Home</Nav.Link>
+                            <Nav.Link href="/search">Hispanic</Nav.Link>
+                            <Nav.Link href="/search">New Student</Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
                 <Nav className="">
+                    <Navbar.Brand href="/">Home</Navbar.Brand>
                     <Navbar.Brand href="/userlogin">Log In</Navbar.Brand>
                     <Navbar.Brand href="/login">Sign Up</Navbar.Brand>
                 </Nav>

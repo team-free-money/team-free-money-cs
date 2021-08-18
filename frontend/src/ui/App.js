03af7895adc40fd30a98db3909d8f2c34454fac9
Anais Del campo <adelcampo@cnm.edu>
2021-08-18 16:31:01 -0600

    and more

--- frontend/src/ui/App.js
+++ frontend/src/ui/App.js
@@ -4,9 +4,6 @@ import { Home } from './Home'
 import { FourOhFour } from './FourOhFour'
 import React from 'react'
 import {Search} from './Search'
-
-
-
 import {Navigation} from './Navbar'
 import {ScholarshipSubmission} from "./Scholarship-Submission";
 import {ScholarshipApproval} from "./Scholarship-Approval";
@@ -16,8 +13,6 @@ import "./syle.css"
 import {CreateAccount} from "./Create-Account";
 import {UserLogin} from "./UserLogin";
 import {Footer} from "./Footer";
-
-
 import {PostCard} from "./PostCard";
 
 
@@ -28,16 +23,13 @@ export const App = () => (
             <Header/>
             <Navigation/>
             <Switch>
-<<<<<<< HEAD
 
-=======
-<<<<<<< HEAD
+
+
+
                 <Route exact path='/' component={Home} />
-=======
-<<<<<<< HEAD
->>>>>>> 13632c7d1f3344fb60e32af2d26a232cf4bf280a
+
                 <Route exact path='/home' component={Home} />
->>>>>>> 3f4bb0f748015bd53342f533b21217dba8e3ba51
                 <Route exact path='/login' component={UserLogin} />
                 <Route exact path='/scholarshipsubmission' component={ScholarshipSubmission} />
                 <Route exact path='/scholarshipapproval' component={ScholarshipApproval} />

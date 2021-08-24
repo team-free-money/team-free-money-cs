2e5b33ec10682436c721a61376eca1fa332ac3fd
Jonathan Harada <JHarada@cnm.edu>
2021-08-24 10:38:13 -0600

    updating and pulling

--- frontend/src/ui/Home.js
+++ frontend/src/ui/Home.js
@@ -9,21 +9,20 @@ export const Home = () => {
             <Container as={"section"} className='p-1'>
                 <Row className={"g-0"}>
                     <Col xs={10} md={6} className="d-block mx-auto bg-white p-5">
-                        <Image src={IMG2} fluid className="mx-2 d-block" alt="Mission"/>
-                        <p>
-                            To provide a starting point encouraging first steps for people in the community to seek out access for education,
-                            opening opportunities for improved quality of life.
-                        </p>
+                        <Image height={500} width={500} src={IMG2} fluid className="mx-2 d-block" alt="Mission"/>
                     </Col>
-
+                    <Col className="my-auto fst-italic font-italic">
+                        <h3>
+                            Our goal is to provide a starting point encouraging first steps for people in the community to seek out access for education,
+                            opening opportunities for improved quality of life.
+                        </h3>
+                    </Col>
                 </Row>
             </Container>
             <Container as={"section"} className='mt-3 mb-4 p-2'>
 
                 <Row className={"g-5"}>
-                    <Col xs={10} md={6} className="d-block mx-auto bg-white p-5">
-                        <Image height={275} width={275} src={IMG1} fluid className="mx-auto d-block mb-5" alt="About Us"/>
-                        <h2 className="text-center">About Us</h2>
+                    <Col className="my-auto">
                         <p>
                             Travel time to the nearest starbase? I think you've let your personal feelings cloud your
                             judgement. When has justice ever been as simple as a rule book? About four years. I got tired of
@@ -31,6 +30,11 @@ export const Home = () => {
                             Why don't we just give everybody a promotion and call it a night - 'Commander'? And blowing into
                             maximum warp speed, you appeared for an instant to be in two places at once. Shields up! Rrrrred
                         </p>
+                    </Col>
+                    <Col xs={10} md={6} className="d-block mx-auto bg-white p-5">
+                        <Image height={275} width={275} src={IMG1} fluid className="mx-auto d-block mb-5" alt="About Us"/>
+                        <h2 className="text-center">About Us</h2>
+
                     </Col>
 
                 </Row>

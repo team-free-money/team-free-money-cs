1b11492b89e6339ed706f1f584f0e4ce2db504b7
Anais Del campo <adelcampo@cnm.edu>
2021-08-25 16:11:19 -0600

    new

--- frontend/src/ui/Footer.js
+++ frontend/src/ui/Footer.js
@@ -3,15 +3,15 @@ import React from "react";
 export const Footer = () => {
     return (
         <>
-<<<<<<< HEAD
 
-            <footer></footer>
+
+
 
             <footer className="bg-dark text-center p-5" >Hello</footer>
 
-=======
-            <footer className="bg-dark text-center p-5" >Hello</footer>
->>>>>>> 9b4e9ec443d4765f6d648eb8b357444b6e58a894
+
+
+
         </>
     )
 }

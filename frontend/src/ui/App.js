a563b33894c0c86a26ab2c62f28cb0c8b0bea4d0
Anais Del campo <adelcampo@cnm.edu>
2021-08-25 16:02:19 -0600

    update

--- frontend/src/ui/App.js
+++ frontend/src/ui/App.js
@@ -3,7 +3,6 @@ import { Route, Switch } from 'react-router'
 import { Home } from './Home'
 import { FourOhFour } from './FourOhFour'
 import React from 'react'
-<<<<<<< HEAD
 import {Search} from './Search'
 import {Navigation} from './Navbar'
 import {ScholarshipSubmission} from "./Scholarship-Submission";
@@ -15,10 +14,8 @@ import {CreateAccount} from "./Create-Account";
 import {UserLogin} from "./UserLogin";
 import {Footer} from "./Footer";
 import {PostCard} from "./PostCard";
-
-=======
 import {TopScholarship} from './Top-Scholarship'
->>>>>>> Top-Scholarship
+
 
 export const App = () => (
     <>
@@ -32,8 +29,6 @@ export const App = () => (
 
 
                 <Route exact path='/' component={Home} />
-<<<<<<< HEAD
-
                 <Route exact path='/home' component={Home} />
                 <Route exact path='/login' component={UserLogin} />
                 <Route exact path='/scholarshipsubmission' component={ScholarshipSubmission} />
@@ -47,13 +42,9 @@ export const App = () => (
                 <Route exact path='/' component={Home} />
                 <Route exact path='/search' component={Search} />
                 <Route exact path='/fourohfour' component={FourOhFour} />
-
                 <Route component={FourOhFour} />
-
-=======
                 <Route exact path='/topscholarship' component={TopScholarship} />
                 <Route exact path='/fourohfour' component={FourOhFour} />
->>>>>>> Top-Scholarship
             </Switch>
             <Footer/>
         </BrowserRouter>

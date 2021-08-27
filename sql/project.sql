9dbcc9cade3b6d0abb19dc91bfa0b7e6315d5ec8
gabriela-reyes <86327609+gabriela-reyes@users.noreply.github.com>
2021-08-27 11:01:24 -0600

    creating tables

--- sql/project.sql
+++ sql/project.sql
@@ -1,41 +1,51 @@
-drop table if exists articleTag;
-drop table if exists article;
-drop table if exists tag;
-drop table if exists author;
-create table author(
-                       authorId binary(16) not null,
-                       authorActivationToken char(32),
-                       authorAvatarUrl varchar(255),
-                       authorEmail varchar(128) not null,
-                       authorHash char(97) not null,
-                       authorUsername varchar(32) not null,
-                       unique(authorEmail),
-                       unique(authorUsername),
-                       index(authorEmail),
-                       primary key(authorId)
+drop table if exists `like`;
+drop table if exists scholarshipCategory;
+drop table if exists category;
+drop table if exists `user`;
+drop table if exists scholarship;
+
+create table scholarship(
+                        scholarshipId binary(16) not null primary key,
+                        scholarshipApproved boolean,
+                        scholarshipAmount varchar(8) null,
+                        scholarshipCriteria varchar(2000) null,
+                        scholarshipDeadline varchar(32) null,
+                        scholarshipDescription varchar(2000) null,
+                        scholarshipLink varchar(128) null,
+                        scholarshipName varchar(128) null,
+                        index(scholarshipId)
 );
-create table tag(
-                    tagId binary(16) not null,
-                    tagName varchar(32) not null,
-                    primary key(tagId)
+
+create table `user`(
+                    userId binary(16) not null primary key,
+                    userActivationToken char(32),
+                    userAdmin varchar(32) not null,
+                    userEmail varchar(128) not null unique,
+                    userHash char(97) not null,
+                    userName varchar(32) not null unique,
+                    index(userEmail)
 );
-create table article(
-                        articleId binary(16) not null,
-                        articleAuthorId binary(16) not null,
-                        articleContent blob not null,
-                        articleDate datetime(6) not null,
-                        articleImage varchar(255),
-                        index(articleAuthorId),
-                        foreign key(articleAuthorId) references author(authorId),
-                        primary key(articleId)
+
+create table category(
+                        categoryId binary(16) not null primary key,
+                        categoryName varchar(32) not null,
+                        index(categoryId)
 );
-create table articleTag(
-                           articleTagArticleId binary(16),
-                           articleTagTagId binary(16),
-                           index(articleTagArticleId),
-                           index(articleTagTagId),
-                           foreign key(articleTagArticleId) references article(articleId),
-                           foreign key(articleTagTagId) references tag(tagId),
-                           primary key(articleTagArticleId, articleTagTagId)
+
+create table scholarshipCategory(
+                           scholarshipCategoryCategoryId binary(16) not null,
+                           scholarshipCategoryScholarshipId binary(16) not null,
+                           foreign key(scholarshipCategoryCategoryId) references category(categoryId),
+                           foreign key(scholarshipCategoryScholarshipId) references scholarship(scholarshipId),
+                           primary key (scholarshipCategoryCategoryId, scholarshipCategoryScholarshipId)
+
 );
- Drop TABLE author;
+
+create table `like`(
+                    likeScholarshipId binary(16) not null,
+                    likeUserId binary(16) not null,
+                    foreign key (likeScholarshipId) references scholarship(scholarshipId),
+                    foreign key (likeUserId) references user(userId),
+                    primary key (likeScholarshipId, likeUserId)
+
+)

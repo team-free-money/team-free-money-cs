81ae896ef883589867f7975a7602f321b654971b
Komal Chaudhary <kchaudhary1@cnm.edu>
2021-08-27 09:02:07 -0600

    quick change on home page

--- frontend/src/ui/Home.js
+++ frontend/src/ui/Home.js
@@ -9,14 +9,14 @@ export const Home = () => {
 
             <Container as={"section"} className='mt-3 mb-4 p-2' id="About">
 
-                <Row className={"g-5"}>
+                <Row className="g-5">
                     <Col xs={10} md={6} className="d-block mx-auto bg-white p-5 bg-transparent">
                         <Image height={275} width={275} src={IMG1} fluid className="mx-auto d-block mb-5" alt="About Us"/>
 
                     </Col>
                     <Col xs={10} md={6} className="my-auto my-auto fst-italic font-italic">
                         <h2 className="text-center">About Us</h2>
-                        <h3>
+                        <h3 className="text-center">
                             We are Full stack Web Developer Intern at Deep Dive Coding. We build this website as our capstone project.
                         </h3>
                     </Col>
@@ -27,8 +27,8 @@ export const Home = () => {
                                 <Col xs={10} md={6} className="d-block mx-auto bg-white p-5 bg-transparent">
                                     <Image height={500} width={500} src={IMG2} fluid className="mx-2 d-block" alt="Mission"/>
                                 </Col>
-                                <Col className="my-auto fst-italic font-italic">
-                                    <h3>
+                                <Col xs={10} md={6} className="my-auto fst-italic font-italic">
+                                    <h3 className="text-center">
                                         Our goal is to provide a starting point encouraging first steps for people in the community to seek out access for education,
                                         opening opportunities for improved quality of life.
                                     </h3>

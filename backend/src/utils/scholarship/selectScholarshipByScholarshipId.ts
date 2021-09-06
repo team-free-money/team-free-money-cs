05c7710377dc9a2a49b60adcb26f696661b34491
Komal Chaudhary <kchaudhary1@cnm.edu>
2021-09-06 17:56:46 -0600

    added mySQL function for selectScholarshipByscholarshipId

---
+++ backend/src/utils/scholarship/selectScholarshipByScholarshipId.ts
@@ -0,0 +1,16 @@
+import {Scholarship} from "../interfaces/Scholarship";
+import  {connect} from "../database.utils";
+import {RowDataPacket} from "mysql2";
+
+export async function selectScholarshipByScholarshipId(): Promise<Scholarship|null> {
+    try {
+        const mySqlConnection = await connect();
+        const mySqlQuery : string = "SELECT BIN_TO_UUID(scholarshipId) as scholarshipId, scholarshipAmount, scholarshipCriteria, scholarshipDeadline, scholarshipDescription, scholarshipLink, scholarshipName FROM scholarship WHERE scholarshipId = BIN_TO_UUID(scholarshipId)"
+        const result = await mySqlConnection.execute(mySqlQuery)as RowDataPacket[]
+        // @ts-ignore
+        return result[0] as Scholarship[]
+    }
+    catch (error){
+        throw error
+    }
+}

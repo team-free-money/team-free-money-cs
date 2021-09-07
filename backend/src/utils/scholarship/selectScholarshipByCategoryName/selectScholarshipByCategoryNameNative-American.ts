a172dbb048acb0c520afa7436e0d9b5d2e6b41c9
Komal Chaudhary <kchaudhary1@cnm.edu>
2021-09-07 07:12:31 -0600

    added mySql Statement for selecting all scholarship by their category name

--- backend/src/utils/scholarship/selectScholarshipByCategoryName/selectScholarshipByCategoryNameNative-American.ts
+++ backend/src/utils/scholarship/selectScholarshipByCategoryName/selectScholarshipByCategoryNameNative-American.ts
@@ -2,11 +2,11 @@ import {Scholarship} from "../../interfaces/Scholarship";
 import {connect} from "../../database.utils";
 import {RowDataPacket} from 'mysql2';
 
-export async function selectTweetsByTweetProfileId(tweetProfileId: string) : Promise<Scholarship[]> {
+export async function selectScholarshipByCategoryNameNativeAmerican(categoryName: string) : Promise<Scholarship[]> {
     try {
         const mySqlConnection = await connect();
-        const mySqlQuery = "SELECT categoryName, BIN_TO_UUID(scholarshipId) as scholarshipId, scholarshipAmount, scholarshipCriteria, scholarshipDeadline, scholarshipDescription, scholarshipLink, scholarshipName FROM category INNER JOIN scholarshipCategory ON scholarshipCategory.scholarshipCategoryCategoryId = categoryID INNER JOIN scholarship ON scholarshipCategory.scholarshipCategoryScholarshipId = scholarship.scholarshipId WHERE categoryName = 'Native-American'";
-        const result = await <RowDataPacket>mySqlConnection.execute(mySqlQuery, {tweetProfileId})
+        const mySqlQuery = "SELECT BIN_TO_UUID(scholarshipId) as scholarshipId, scholarshipAmount, scholarshipCriteria, scholarshipDeadline, scholarshipDescription, scholarshipLink, scholarshipName FROM category INNER JOIN scholarshipCategory ON scholarshipCategory.scholarshipCategoryCategoryId = categoryID INNER JOIN scholarship ON scholarshipCategory.scholarshipCategoryScholarshipId = scholarship.scholarshipId WHERE categoryName = 'Native-American'";
+        const result = await <RowDataPacket>mySqlConnection.execute(mySqlQuery, {categoryName})
         return result[0] as Scholarship[]
     } catch (error) {
         throw error

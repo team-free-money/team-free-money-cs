796c68f2db42cc28b733d020cb552e46ac2fdebf
Jonathan Harada <JHarada@cnm.edu>
2021-09-10 15:48:28 -0600

    fixed dynamic data for categories

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,7 +1,8 @@
 import scholarships from "./scholarships"
 import categories from "./category"
+import scholarshipCategory from "./scholarshipCategory";
 import {combineReducers, configureStore} from "@reduxjs/toolkit";
 
-const reducer =  combineReducers({scholarships, categories})
+const reducer =  combineReducers({scholarships, categories, scholarshipCategory})
 
 export const store = configureStore({reducer})

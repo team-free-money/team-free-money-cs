d874e7f08778721662ff31934b29b4d6481eba4b
gabriela-reyes <86327609+gabriela-reyes@users.noreply.github.com>
2021-09-13 14:04:54 -0600

    working on badge/likeCounter

--- frontend/src/ui/shared/SearchCard.js
+++ frontend/src/ui/shared/SearchCard.js
@@ -1,9 +1,29 @@
 import React from "react";
 import {Badge, Button, Card, Col, Row} from "react-bootstrap";
 import {Star} from "react-bootstrap-icons";
+import {httpConfig} from "../../utils/httpConfig";
+import like, {fetchAllLikes} from "../../store/like";
+import scholarships, {fetchAllScholarships} from "../../store/scholarships";
+import {useDispatch} from "react-redux";
 
 export function SearchCard (props) {
+    const dispatch = useDispatch()
+    const initialEffects = () => {
+        dispatch(fetchAllLikes())
+}
     const {scholarship} = props
+
+    const clickLike = () => {
+        httpConfig.post('/apis/like/', {likeScholarshipId: scholarship.scholarshipId})
+            .then(reply => {
+                    if (reply.status === 200) {
+                        console.log(reply);
+                        dispatch(fetchAllLikes());
+                    }
+                    console.log(reply);
+                }
+            )};
+
     return(
         <>
             <Card>
@@ -25,9 +45,10 @@ export function SearchCard (props) {
                         </Card.Body>
                     </Col>
                     <Col>
-                        <Button variant="info" style={{marginLeft: "auto", display:"block"}}>
-                            #likes <Badge bg="secondary">9</Badge>
+                        <Button onClick = {clickLike} variant="info" style={{marginLeft: "auto", display:"block"}}>
+                            <Badge bg="secondary">{scholarship.likeCount}</Badge>Save
                             <span className="visually-hidden"></span>
+
                         </Button>
                     </Col>
                 </Row>
